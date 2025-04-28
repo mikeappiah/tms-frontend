928adef8c75662cd996e1bdfa5ef56796f68ed8b
Michael Appiah <mke.appiah@gmail.com>
2025-04-28 09:56:45 +0000

    Fix package.json dependency errors

--- src/app/user/notifications/page.tsx
+++ src/app/user/notifications/page.tsx
@@ -1,28 +1,28 @@
-"use client";
+'use client';
 
-import { PageHeader } from "@/components/header";
-import NotificationItem from "@/components/notifications/NotificationItem";
-import notificationsData from "@/data/notificationsData";
+import { PageHeader } from '@/components/header';
+import NotificationItem from '@/components/notifications/NotificationItem';
+import notificationsData from '@/data/notificationsData';
 
 const NotificationsDashboard: React.FC = () => {
-  return (
-    <div>
-      <div>
-        <PageHeader
-          title="Notifications"
-          description="View your recent notifications"
-        />
-        <div className="bg-white rounded-md p-6 mt-10">
-          {notificationsData.map((notification) => (
-            <NotificationItem
-              key={notification.id}
-              notification={notification}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div>
+				<PageHeader
+					title='Notifications'
+					description='View your recent notifications'
+				/>
+				<div className='bg-white rounded-md p-6 mt-10'>
+					{notificationsData.map((notification) => (
+						<NotificationItem
+							key={notification.id}
+							notification={notification}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default NotificationsDashboard;
